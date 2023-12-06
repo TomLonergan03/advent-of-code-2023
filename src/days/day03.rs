16e416f63fb238be7ff8a7635ccc7fbfe0955278
Tom Lonergan <tomlonergan91@gmail.com>
2023-12-06 23:31:04 +0000

    day 3

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,4 +1,4 @@
-use std::cmp::min;
+use crate::utils::{get_neighbours, NeighbourPattern};
 
 pub fn _run(input: Vec<String>) {
     println!("Part 1: {}", part_1(&input));
@@ -12,31 +12,33 @@ fn part_1(input: &[String]) -> u32 {
         .map(|s| s.chars().collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
     for i in 0..chars.len() {
-        let mut can_start_number = true;
         for j in 0..chars[0].len() {
             if chars[i][j] == '.' {
-                can_start_number = true;
                 continue;
             }
-            if chars[i][j].is_numeric() && can_start_number {
-                let (result, start, end) = build_num(&chars, i, j);
-                // println!("{} {}", start, end);
-                for k in i.saturating_sub(1)..min(i + 2, chars.len()) {
-                    for l in start.saturating_sub(1)..min(end + 1, chars[0].len()) {
-                        if chars[k][l] != '.' && !chars[k][l].is_numeric() {
-                            // println!("including {} at {k} {l}", result);
-                            sum += result;
-                        }
+            if !chars[i][j].is_numeric() {
+                let neighbours =
+                    get_neighbours(i, j, chars.len(), chars[0].len(), NeighbourPattern::All);
+                let mut can_start_num = 0;
+                let mut current_row = 0;
+                for (x, y) in neighbours {
+                    if x > current_row {
+                        current_row = x;
+                        can_start_num = 0;
+                    }
+                    if chars[x][y].is_numeric() && (y >= can_start_num || x > current_row) {
+                        let (number, end) = build_num(&chars, x, y);
+                        can_start_num = end;
+                        sum += number
                     }
                 }
-                can_start_number = false;
             }
         }
     }
     sum
 }
 
-fn build_num(array: &[Vec<char>], i: usize, j: usize) -> (u32, usize, usize) {
+fn build_num(array: &[Vec<char>], i: usize, j: usize) -> (u32, usize) {
     let mut start = j;
     while start > 0 {
         if !array[i][start].is_numeric() {
@@ -57,9 +59,42 @@ fn build_num(array: &[Vec<char>], i: usize, j: usize) -> (u32, usize, usize) {
             break;
         }
     }
-    (result, start, end)
+    (result, end)
 }
 
-fn part_2(_input: &[String]) -> i32 {
-    -1
+fn part_2(_input: &[String]) -> u32 {
+    let mut sum = 0;
+    let chars = _input
+        .iter()
+        .map(|s| s.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+    for i in 0..chars.len() {
+        for j in 0..chars[0].len() {
+            if chars[i][j] == '.' {
+                continue;
+            }
+            if chars[i][j] == '*' {
+                let neighbours =
+                    get_neighbours(i, j, chars.len(), chars[0].len(), NeighbourPattern::All);
+                let mut can_start_num = 0;
+                let mut current_row = 0;
+                let mut surrounding_nums = Vec::new();
+                for (x, y) in neighbours {
+                    if x > current_row {
+                        current_row = x;
+                        can_start_num = 0;
+                    }
+                    if chars[x][y].is_numeric() && (y >= can_start_num || x > current_row) {
+                        let (number, end) = build_num(&chars, x, y);
+                        can_start_num = end;
+                        surrounding_nums.push(number)
+                    }
+                }
+                if surrounding_nums.len() == 2 {
+                    sum += surrounding_nums[0] * surrounding_nums[1]
+                }
+            }
+        }
+    }
+    sum
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,1 +1,58 @@
+#[derive(PartialEq)]
+pub enum NeighbourPattern {
+    Plus,
+    PlusWithCenter,
+    All,
+    AllWithCenter,
+}
 
+pub fn get_neighbours(
+    x: usize,
+    y: usize,
+    width: usize,
+    height: usize,
+    pattern: NeighbourPattern,
+) -> Vec<(usize, usize)> {
+    let x = x as i32;
+    let y = y as i32;
+    let mut result: Vec<(usize, usize)> = Vec::new();
+    match pattern {
+        NeighbourPattern::All => {
+            for i in x - 1..=x + 1 {
+                for j in y - 1..=y + 1 {
+                    if i >= 0 && i < height as i32 && j <= width as i32 && !(i == x && j == y) {
+                        result.push((i as usize, j as usize))
+                    }
+                }
+            }
+        }
+        NeighbourPattern::AllWithCenter => {
+            for i in x - 1..=x + 1 {
+                for j in y - 1..=y + 1 {
+                    if i >= 0 && i < height as i32 && j <= width as i32 {
+                        result.push((i as usize, j as usize))
+                    }
+                }
+            }
+        }
+        NeighbourPattern::Plus => {
+            for i in x - 1..=x + 1 {
+                for j in y - 1..=y + 1 {
+                    if i >= 0 && i < height as i32 && j <= width as i32 && (i != x && j != y) {
+                        result.push((i as usize, j as usize))
+                    }
+                }
+            }
+        }
+        NeighbourPattern::PlusWithCenter => {
+            for i in x - 1..=x + 1 {
+                for j in y - 1..=y + 1 {
+                    if i >= 0 && i < height as i32 && j <= width as i32 && (i == x || j == y) {
+                        result.push((i as usize, j as usize))
+                    }
+                }
+            }
+        }
+    }
+    result
+}
