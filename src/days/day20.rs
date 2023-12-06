b1db287fa3ae72560762f29e0069a8d381ba9a28
Tom Lonergan <tomlonergan91@gmail.com>
2023-12-06 15:29:01 +0000

    all days starter file

---
+++ src/days/day20.rs
@@ -0,0 +1,13 @@
+pub fn _run(input: Vec<String>) {
+    println!("Part 1: {}", _part_1(&input));
+    println!("Part 2: {}", _part_2(&input));
+}
+
+fn _part_1(_input: &[String]) -> i32 {
+    -1
+}
+
+fn _part_2(_input: &[String]) -> i32 {
+    -1
+}        
+        
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,8 @@
 use std::{env::args, time::Instant};
 
-mod day1;
-mod day2;
-mod day3;
-mod day4;
-// mod day5;
-// mod day6;
-// mod day7;
-// mod day8;
-// mod day9;
-// mod day10;
-// mod day11;
-// mod day12;
-// mod day13;
-// mod day14;
-// mod day15;
-// mod day16;
-// mod day17;
-// mod day18;
-// mod day19;
-// mod day20;
-// mod day21;
-// mod day22;
-// mod day23;
-// mod day24;
-// mod day25;
+use chrono::Datelike;
+
+mod days;
 mod input_handling;
 mod utils;
 
@@ -43,16 +21,13 @@ fn main() {
     println!("{SEPARATOR}");
     let parsed_args: ParsedArgs = parse_args(args().collect::<Vec<_>>().as_slice());
     if parsed_args.day != 0 {
-        let _ = run_task(parsed_args.day, &parsed_args);
+        run_task(parsed_args.day, &parsed_args);
         println!("{SEPARATOR}");
         return;
     }
-    for i in 1..=25 {
-        let result = run_task(i, &parsed_args);
+    for i in 1..=chrono::offset::Utc::now().day() as u8 {
+        run_task(i, &parsed_args);
         println!("{SEPARATOR}");
-        if result.is_err_and(|x| x.to_string() == "Day not released yet") {
-            break;
-        }
     }
 }
 
@@ -82,41 +57,10 @@ fn parse_args(args: &[String]) -> ParsedArgs {
     }
 }
 
-fn run_task(day_number: u8, args: &ParsedArgs) -> Result<(), Box<dyn std::error::Error>> {
+fn run_task(day_number: u8, args: &ParsedArgs) {
     println!("Day {}", day_number);
-    let input = input_handling::get_input(day_number, args.use_test)?;
+    let input = input_handling::get_input(day_number, args.use_test).unwrap();
     let now = Instant::now();
-    match day_number {
-        1 => day1::run(input),
-        2 => day2::run(input),
-        3 => day3::run(input),
-        4 => day4::run(input),
-        // 5 => day5::run(input),
-        // 6 => day6::run(input),
-        // 7 => day7::run(input),
-        // 8 => day8::run(input),
-        // 9 => day9::run(input),
-        // 10 => day10::run(input),
-        // 11 => day11::run(input),
-        // 12 => day12::run(input),
-        // 13 => day13::run(input),
-        // 14 => day14::run(input),
-        // 15 => day15::run(input),
-        // 16 => day16::run(input),
-        // 17 => day17::run(input),
-        // 18 => day18::run(input),
-        // 19 => day19::run(input),
-        // 20 => day20::run(input),
-        // 21 => day21::run(input),
-        // 22 => day22::run(input),
-        // 23 => day23::run(input),
-        // 24 => day24::run(input),
-        // 25 => day25::run(input),
-        _ => {
-            println!("Day {} not implemented", day_number);
-            return Ok(());
-        }
-    }
+    days::run_day(day_number, input);
     println!("Day {} completed in: {:.2?}", day_number, now.elapsed());
-    Ok(())
 }
