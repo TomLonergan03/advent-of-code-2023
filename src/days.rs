6dffa5bc1eaa178cac6a562d7a7645f2b77e0ecb
Tom Lonergan <tomlonergan91@gmail.com>
2023-12-09 14:10:16 +0000

    day 9

--- src/days.rs
+++ src/days.rs
@@ -34,7 +34,7 @@ pub fn run_day(day_number: u8, input: Vec<String>) {
         6 => day06::_run(input),
         7 => day07::_run(input),
         8 => day08::_run(input),
-        // 9 => day09::_run(input),
+        9 => day09::_run(input),
         // 10 => day10::_run(input),
         // 11 => day11::_run(input),
         // 12 => day12::_run(input),
--- src/days/day09.rs
+++ src/days/day09.rs
@@ -3,10 +3,41 @@ pub fn _run(input: Vec<String>) {
     println!("Part 2: {}", _part_2(&input));
 }
 
+fn find_next(sequence: Vec<i32>) -> i32 {
+    let mut steps = vec![sequence];
+    while steps.last().unwrap().iter().any(|&x| x != 0) {
+        steps.push(
+            steps
+                .last()
+                .unwrap()
+                .iter()
+                .zip(steps.last().unwrap().iter().skip(1))
+                .map(|(x, y)| y - x)
+                .collect(),
+        )
+    }
+    for i in (0..steps.len() - 1).rev() {
+        let new_value = steps[i + 1].last().unwrap() + steps[i].last().unwrap();
+        steps[i].push(new_value)
+    }
+    *steps[0].last().unwrap()
+}
+
 fn _part_1(_input: &[String]) -> i32 {
-    -1
+    _input
+        .iter()
+        .map(|x| x.split_whitespace().map(|y| y.parse().unwrap()).collect())
+        .fold(0, |acc, x| acc + find_next(x))
 }
 
 fn _part_2(_input: &[String]) -> i32 {
-    -1
+    _input
+        .iter()
+        .map(|x| {
+            x.split_whitespace()
+                .map(|y| y.parse().unwrap())
+                .rev()
+                .collect()
+        })
+        .fold(0, |acc, x| acc + find_next(x))
 }
